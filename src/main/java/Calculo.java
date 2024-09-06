f7e493f4cfc59c96f64f7dcbd0616d3971db3f87
Guilherme Santos <80459748+Guilermiz@users.noreply.github.com>
2024-09-05 22:19:28 -0300

    Update Calculo.java

--- src/main/java/Calculo.java
+++ src/main/java/Calculo.java
@@ -14,41 +14,45 @@ public class Calculo {
 
     private double liquido;
 
+    // método que calcula desconto do INSS do salário bruto
     public double descontoInss(double bruto){
         double inss = 0;
         if(bruto <= 1412.2){
             inss = bruto * 0.075;
         }else if (bruto <= 2666.68) {
-            inss = bruto * 0.09 - 21.18;
+            inss = (bruto * 0.09) - 21.18;
         } else if (bruto <= 4000.03) {
-            inss = bruto * 0.12 - 101.18;
+            inss = (bruto * 0.12) - 101.18;
         } else if (bruto <= 7786.02) {
-            inss = bruto * 0.14 - 181.18;
+            inss = (bruto * 0.14) - 181.18;
         } else {
-            inss = 7786.02 * 0.14 - 181.18;
+            inss = (7786.02 * 0.14) - 181.18;
         }
         return inss;
     }
+
+    // método que calcula desconto do IR do salário bruto
     public double descontoIr(double bruto) {
         double ir = 0;
         if (bruto <= 1903.98) {
             ir = 0;
         } else if (bruto <= 2826.65) {
-            ir = bruto * 0.075 - 142.80;
+            ir = (bruto * 0.075) - 142.80;
         } else if (bruto <= 3751.05) {
-            ir = bruto * 0.15 - 354.80;
+            ir = (bruto * 0.15) - 354.80;
         } else if (bruto <= 4664.68) {
-            ir = bruto * 0.225 - 636.13;
+            ir = (bruto * 0.225) - 636.13;
         } else {
-            ir = bruto * 0.275 - 869.36;
+            ir = (bruto * 0.275) - 869.36;
         }
         return ir;
     }
 
+    // método que calcula o salário líquido
     public void salarioLiquido(double bruto) {
         double inss = descontoInss(bruto);
-        double ir = descontoIr(bruto - inss);
-        double salario = bruto - inss - ir;
+        double ir = descontoIr(bruto);
+        double salario = bruto - (inss + ir);
         setLiquido(salario);
     }
 }
